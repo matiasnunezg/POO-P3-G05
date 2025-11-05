9e668d7093e088b7a7d2c3bf995958e7c75de5a8
aaronaugus06-netizen <aaronaugus06@gmail.com>
2025-11-04 21:37:03 -0500

    Create RegistrarHorasDeSuenio.java
    Ya sabe chambeando mientras otros Zzz

---
+++ Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/modelo/RegistrarHorasDeSuenio.java
@@ -0,0 +1,58 @@
+package espol.poo.modelo;
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.Duration;
+
+public class RegistrarHorasDeSuenio {
+
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private double duracionHoras;
+    private double metaRecomendada = 8.0;
+    private LocalDate fechaRegistro;
+
+    public RegistrarHorasDeSuenio(LocalTime horaInicio, LocalTime horaFin, LocalDate fechaRegistro) {
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.fechaRegistro = fechaRegistro;
+        calcularDuracion();
+    }
+
+    public void calcularDuracion(){
+        Duration duracion;
+        if( horaFin.isBefore(horaInicio)){
+            duracion = Duration.between(horaInicio, horaFin.plusHours(24));
+        }
+        else{
+            duracion = Duration.between(horaInicio, horaFin);
+        }
+        duracionHoras = duracion.toMinutes() / 60.0;
+    }
+
+    public boolean cumpleMeta() {
+        return duracionHoras >= metaRecomendada;
+    }
+
+    public String mostrarResumen() {
+        String resultado = String.format("""
+        --- RESUMEN DE HOY ---
+        Sueño registrado: %.1fh
+        Recomendación estándar: %.1fh
+        Meta alcanzada: %s
+        """, duracionHoras, metaRecomendada, (cumpleMeta() ? "SÍ " : "NO"));
+        return resultado;
+    }
+
+    public double getDuracionHoras() { 
+        return duracionHoras; 
+    }
+
+    public LocalDate getFechaRegistro() { 
+        return fechaRegistro; 
+    }
+}
+
+
+
+
+
