76b32a7a14c47371ea9fef4ab4f1fb20be60cd25
Sergtumb <gamer.anime.sath@gmail.com>
2025-11-11 19:42:48 -0500

    .

--- Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/modelo/TecnicasEnfoque.java
+++ Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/modelo/TecnicasEnfoque.java
@@ -1,5 +1,74 @@
 package espol.poo.modelo;
 
+import espol.poo.vista.VistaEnfoque;
+import espol.poo.controlador.ControladorEnfoque;
+import java.util.Scanner;
+
 public class TecnicasEnfoque {
 
+    private VistaEnfoque vista;
+    private ControladorEnfoque ctrl;
+
+    public TecnicasEnfoque() {
+        Scanner sc = new Scanner(System.in);
+        this.vista = new VistaEnfoque();
+        this.ctrl = new ControladorEnfoque(sc);
+    }
+
+    public void mostrarMenu() {
+        int opcion = 0;
+
+        while (opcion != 3) {
+            vista.mostrarMenuPrincipal();
+            opcion = ctrl.obtenerOpcionMenu();
+
+            switch (opcion) {
+                case 1 -> iniciarPomodoro();
+                case 2 -> iniciarDeepWork();
+                case 3 -> vista.mostrarVolver();
+                default -> vista.mostrarOpcionInvalida();
+            }
+        }
+    }
+
+    private void iniciarPomodoro() {
+        vista.mostrarInicioPomodoro();
+        vista.mostrarSeleccionActividad();
+        String input = ctrl.obtenerActividad();
+        if (input.equals("0")) return;
+
+        int ciclos = 4;
+        for (int i = 1; i <= ciclos; i++) {
+            vista.mostrarPomodoroCiclo(i, ciclos);
+            ctrl.esperarEnter();
+
+            vista.mostrarFinTrabajo();
+
+            if (i < ciclos) {
+                vista.mostrarDescanso();
+                ctrl.esperarEnter();
+                vista.mostrarFinDescanso();
+                ctrl.esperarEnter();
+            }
+        }
+        vista.mostrarPomodoroFinal();
+    }
+
+    private void iniciarDeepWork() {
+        vista.mostrarInicioDeepWork();
+        vista.mostrarSeleccionActividad();
+        String input = ctrl.obtenerActividad();
+        if (input.equals("0")) return;
+
+        vista.mostrarDeepWorkSesion();
+        ctrl.esperarEnter();
+        vista.mostrarDeepWorkFinal();
+    }
+
+    public static void main(String[] args) {
+        TecnicasEnfoque app = new TecnicasEnfoque();
+        app.mostrarMenu();
+        
+    }
+    
 }
--- Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/vista/VistaEnfoque.java
+++ Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/vista/VistaEnfoque.java
@@ -1,5 +1,75 @@
 package espol.poo.vista;
 
 public class VistaEnfoque {
+    public void mostrarMenuPrincipal() {
+        System.out.println("\n--- T É C N I C A S   D E   E N F O Q U E ---");
+        System.out.println("1. Iniciar Pomodoro (25 min Trabajo / 5 min Descanso)");
+        System.out.println("2. Iniciar Deep Work (Sesión Larga de 90 min)");
+        System.out.println("3. Volver al Menú Principal");
+        System.out.print("Ingrese su opción: ");
+    }
+
+    public void mostrarSeleccionActividad() {
+        System.out.println("\nSeleccione la Actividad a trabajar:");
+        System.out.println("ID | TIPO | NOMBRE");
+        System.out.println("1 | TAREA     | Cuestionario Unidad 2");
+        System.out.println("2 | PROYECTO  | Sistema Gestión Tiempo (Fase 1)");
+        System.out.print("\nIngrese ID de la actividad (o 0 para salir): ");
+        
+    }
+
+    public void mostrarOpcionInvalida() {
+        System.out.println("Opción no válida. Intente nuevamente.");
+    }
+
+    public void mostrarVolver() {
+        System.out.println("Volviendo al menú principal...");
+    }
+
+    
+    public void mostrarInicioPomodoro() {
+        System.out.println("\n--- INICIAR POMODORO ---");
+    }
+
+    public void mostrarPomodoroCiclo(int ciclo, int total) {
+        System.out.printf("Técnica: Pomodoro | Ciclo: %d/%d\n", ciclo, total);
+        System.out.println("Tiempo de Trabajo: 25:00 minutos restantes");
+        System.out.println("[Presione ENTER para finalizar el trabajo]");
+    }
+
+    public void mostrarFinTrabajo() {
+        System.out.println("\n--- ¡TIEMPO DE TRABAJO TERMINADO! ---");
+        System.out.println("Sesión registrada (avance de la actividad actualizado).");
+    }
+
+    public void mostrarDescanso() {
+        System.out.println("\nTiempo de descanso: 05:00 minutos");
+        System.out.println("[Presione ENTER para iniciar el descanso]");
+    }
+
+    public void mostrarFinDescanso() {
+        System.out.println("[Descanso finalizado, presione ENTER para continuar]");
+    }
+
+    public void mostrarPomodoroFinal() {
+        System.out.println("\n--- SESIÓN POMODORO COMPLETADA ---");
+        System.out.println("El uso de la técnica Pomodoro ha sido registrado correctamente.");
+    }
+
+    
+    public void mostrarInicioDeepWork() {
+        System.out.println("\n--- INICIAR DEEP WORK ---");
+    }
+
+    public void mostrarDeepWorkSesion() {
+        System.out.println("Técnica: Deep Work");
+        System.out.println("Tiempo de trabajo: 90:00 minutos restantes");
+        System.out.println("[Presione ENTER para finalizar la sesión]");
+    }
+
+    public void mostrarDeepWorkFinal() {
+        System.out.println("\n--- ¡SESIÓN DEEP WORK COMPLETADA! ---");
+        System.out.println("Sesión registrada (avance actualizado en base al tiempo trabajado).");
+    }
 
 }
